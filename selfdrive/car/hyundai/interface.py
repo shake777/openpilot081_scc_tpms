6286442384b128307318bb5abc954597d7b0a024
shake777 <jungsanpark@gmail.com>
2021-01-10 20:53:34 +0900

    test 추가

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -132,9 +132,13 @@ class CarInterface(CarInterfaceBase):
 
     ret.lateralTuning.init('lqr')
 
+<<<<<<< Updated upstream
     ret.lateralTuning.lqr.scale = 1800.
+=======
+    ret.lateralTuning.lqr.scale = 1860.
+>>>>>>> Stashed changes
     ret.lateralTuning.lqr.ki = 0.070
-    ret.lateralTuning.lqr.dcGain = 0.002760
+    ret.lateralTuning.lqr.dcGain = 0.002770
 
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
@@ -143,10 +147,14 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.lqr.l = [0.33, 0.318]
 
     ret.steerRatio = 14.20
+<<<<<<< Updated upstream
     ret.steerActuatorDelay = 0.240
+=======
+    ret.steerActuatorDelay = 0.390
+>>>>>>> Stashed changes
     ret.steerLimitTimer = 2.5
 
-    ret.steerRateCost = 0.70
+    ret.steerRateCost = 0.50
 
     ret.steerMaxBP = [0.]
     ret.steerMaxV = [1.2]
